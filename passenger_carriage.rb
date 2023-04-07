ad04bfbdd24cc5c44c58b6c7e66ef890630e483c
mariyamurashova <127765865+mariyamurashova@users.noreply.github.com>
2023-04-07 14:34:50 +0200

    Create passenger_carriage.rb

---
+++ passenger_carriage.rb
@@ -0,0 +1,10 @@
+class PassengerCarriage < Train
+
+  include ProduceCompany
+
+  def initialize(full_carriage_value)
+    @type= :passenger
+    @taken = 0
+    @full_carriage_value = full_carriage_value
+  end
+end
